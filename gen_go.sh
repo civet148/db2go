691c407af030345b2077b437afe620372d48ff45
libin <libin@126.com>
2025-12-31 14:15:17 +0800

    update README

--- gen_go.sh
+++ gen_go.sh
@@ -27,7 +27,19 @@ BASE_MODEL="github.com/civet148/db2go/types.BaseModel=create_time,update_time"
 # 数据库DDL文件
 DDL_FILE="deploy/test.sql"
 
-rm -rf ./models ./dao
+### 检查 db2go 是否已安装
+#if ! which db2go >/dev/null 2>&1; then
+#    # 安装最新版 db2go
+#    go install github.com/civet148/db2go@latest
+#
+#    # 检查是否安装成功
+#    if which db2go >/dev/null 2>&1; then
+#        echo "✅ db2go install success, $(which db2go)"
+#    else
+#        echo "❌ db2go install failed, please check go env and gcc tool-chain"
+#        exit 1
+#    fi
+#fi
 
 ./db2go --url "$DSN_URL" --out "$OUT_DIR" --table "$TABLE_NAME" --json-properties "$JSON_PROPERTIES" --enable-decimal  --spec-type "$SPEC_TYPES" \
  --package "$PACK_NAME" --readonly "$READ_ONLY" --without "$WITH_OUT" --dao dao --tinyint-as-bool "$TINYINT_TO_BOOL" \
