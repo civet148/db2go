32bed90ba1ec511561e47ec668494c01271b856d
libin <libin@126.com>
2026-07-30 13:52:19 +0800

    proto example files

--- gen_protobuf.sh
+++ gen_protobuf.sh
@@ -3,7 +3,7 @@
 OUT_DIR=.
 PACK_NAME="proto"
 WITH_OUT="created_at, updated_at"
-TABLE_NAME="users, classes"
+TABLE_NAME=""
 #DSN_URL="mssql://sa:123456@127.0.0.1:1433/test?instance=SQLEXPRESS&windows=false"
 #DSN_URL="postgres://postgres:123456@127.0.0.1:5432/test?sslmode=disable"
 DSN_URL="mysql://root:123456@127.0.0.1:3306/test?charset=utf8"
